b4b538652bdc24dd4fbe201d27f62429dee2d5ea
Nabil Labrazi <na.labrazi@gmail.com>
2021-04-13 04:22:03 +0200

    hotjar2

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { hotjar } from 'react-hotjar';
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -37,23 +38,8 @@ const App = () => (
       </main>
     </div>
   </div>
-
-// Hotjar Tracking Code for nabil.labrazi.com
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:2346482,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
-
   </Router>
 );
 
-
-
-
+hotjar.initialize(2346482);
 ReactDOM.render(<App />, document.getElementById("root"));
