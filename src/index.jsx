6bdb36f07201119fbfe20f5171a3cfc9177a6450
Nabil Labrazi <na.labrazi@gmail.com>
2021-04-13 04:11:36 +0200

    hotjar

--- src/index.jsx
+++ src/index.jsx
@@ -37,8 +37,23 @@ const App = () => (
       </main>
     </div>
   </div>
+
+// Hotjar Tracking Code for nabil.labrazi.com
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2346482,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
   </Router>
 );
+
+
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
